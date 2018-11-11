d6c81dbb55a5f67d2295e2d5d45f3ccec3c62bb2
leanaAlq <leana.a.q@gmail.com>
2018-11-11 15:24:06 +0300

    rearrange routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   root "pages#home"
 
   devise_for :admins
-  resources :posts, :categories, :comments
-  resources :admins, only: [:show]
+  resources :posts, :categories, :comments, :admins, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
