66007a211877507a84ff2bc7cf7bb9fe9241b47a
leanaAlq <leana.a.q@gmail.com>
2018-11-11 15:09:14 +0300

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get "categories/new"
-  get "categories/show"
-  get "categories/index"
+  # get "categories/new"
+  # get "categories/show"
+  # get "categories/index"
+  root "pages#home"
+
   devise_for :admins
   resources :posts, :categories, :comments
   resources :admins, only: [:show]
-  root "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
